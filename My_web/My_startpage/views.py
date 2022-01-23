2481ffc497df35ff55834aa9c3ccd77eef634950
ChenxuanWei0428 <70415608+ChenxuanWei0428@users.noreply.github.com>
2022-01-23 03:48:28 -0500

    update the startpage of my website

--- My_web/My_startpage/views.py
+++ My_web/My_startpage/views.py
@@ -1,3 +1,17 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from httplib2 import Http
+from django.http import HttpResponse
+from django.shortcuts import render
+# Create your views here
+# 
+# 
+# .
+def start(request, name):
+    return render(request, "startpage\search_page.html", {
+        "name": name.capitalize()
+    })
 
-# Create your views here.
+
+
+

c2f4b0d48f83b6ff5b7341f695da0bb47232d597
ChenxuanWei0428 <70415608+ChenxuanWei0428@users.noreply.github.com>
2022-02-02 23:53:42 -0500

    try template
    update template for 3 different web page

--- My_web/direction/urls.py
+++ My_web/direction/urls.py
@@ -2,6 +2,8 @@ from unicodedata import name
 from django import views
 from django.urls import path
 from . import views
+
+app_name = "direction"
 urlpatterns = [
     path("", views.start, name="start"),
 ]
--- My_web/simple_google/urls.py
+++ My_web/simple_google/urls.py
@@ -2,6 +2,9 @@ from unicodedata import name
 from django import views
 from django.urls import path
 from . import views
+
+
+app_name = "simple_google"
 urlpatterns = [
     path("", views.start, name="start"),
 ]

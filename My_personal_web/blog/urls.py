d7e5c6d64bae664423ff86bb5d882f0de6f88ed0
ChenxuanWei0428 <70415608+ChenxuanWei0428@users.noreply.github.com>
2022-04-25 14:51:15 -0400

    Re organize

--- My_personal_web/blog/urls.py
+++
@@ -1,9 +0,0 @@
-from . import views
-from django.urls import path
-
-app_name = "blog"
-urlpatterns = [
-    path("", views.intro, name="Chenxuan Wei's Blog"),
-    path("cooking", views.cooking, name = "My cooking recipe"),
-    path("still_building", views.still_building, name = "still_building"),
-]
--- My_web_Project/take_a_break/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class MyStartpageConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'take_a_break'
